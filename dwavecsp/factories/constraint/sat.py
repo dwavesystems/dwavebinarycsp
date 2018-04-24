7230c651f78085e6719572f75382a8b41deef65a
Alexander Condello <acondello@dwavesys.com>
2018-04-24 13:54:46 -0700

    Update sat2in4 to use new Constraint.flip_variable method

--- dwavecsp/factories/constraint/sat.py
+++ dwavecsp/factories/constraint/sat.py
@@ -20,55 +20,41 @@ def sat2in4(*args, vartype=dimod.BINARY, name='2-in-4', pos=tuple(), neg=tuple()
         raise ValueError("")
 
     if neg and (len(neg) < 4):
+        # because 2-in-4 sat is symmetric, all negated is the same as none negated
 
-        if vartype is dimod.BINARY:
-            def iter_config():
-                for u, v in itertools.combinations((0, 1, 2, 3), 2):
-                    config = [0] * len(pos) + [1] * len(neg)
-                    config[u] = 1 - config[u]
-                    config[v] = 1 - config[v]
+        const = sat2in4(pos=variables, vartype=vartype)  # make one that has no negations
+        for v in neg:
+            const.flip_variable(v)
+            const.name = name  # overwrite the name directly
 
-                    yield tuple(config)
-            configurations = frozenset(iter_config())
-        else:
-            def iter_config():
-                for u, v in itertools.combinations((0, 1, 2, 3), 2):
-                    config = [-1] * len(pos) + [1] * len(neg)
-                    config[u] *= -1
-                    config[v] *= -1
+        return const
 
-                    yield tuple(config)
-            configurations = frozenset(iter_config())
-
-        return Constraint.from_configurations(configurations=configurations,
-                                              variables=variables, vartype=vartype, name=name)
+    # we can just construct them directly for speed
+    if vartype is dimod.BINARY:
+        configurations = frozenset([(0, 0, 1, 1),
+                                    (0, 1, 0, 1),
+                                    (1, 0, 0, 1),
+                                    (0, 1, 1, 0),
+                                    (1, 0, 1, 0),
+                                    (1, 1, 0, 0)])
     else:
-        # we can just construct them directly for speed
-        if vartype is dimod.BINARY:
-            configurations = frozenset([(0, 0, 1, 1),
-                                        (0, 1, 0, 1),
-                                        (1, 0, 0, 1),
-                                        (0, 1, 1, 0),
-                                        (1, 0, 1, 0),
-                                        (1, 1, 0, 0)])
-        else:
-            # SPIN, vartype is checked by the decorator
-            configurations = frozenset([(-1, -1, +1, +1),
-                                        (-1, +1, -1, +1),
-                                        (+1, -1, -1, +1),
-                                        (-1, +1, +1, -1),
-                                        (+1, -1, +1, -1),
-                                        (+1, +1, -1, -1)])
+        # SPIN, vartype is checked by the decorator
+        configurations = frozenset([(-1, -1, +1, +1),
+                                    (-1, +1, -1, +1),
+                                    (+1, -1, -1, +1),
+                                    (-1, +1, +1, -1),
+                                    (+1, -1, +1, -1),
+                                    (+1, +1, -1, -1)])
 
-        def func(a, b, c, d):
-            if a == b:
-                return (b != c) and (c == d)
-            elif a == c:
-                # a != b
-                return b == d
-            else:
-                # a != b, a != c -> b == c
-                return a == d
+    def func(a, b, c, d):
+        if a == b:
+            return (b != c) and (c == d)
+        elif a == c:
+            # a != b
+            return b == d
+        else:
+            # a != b, a != c -> b == c
+            return a == d
 
     return Constraint(func=func, configurations=configurations, variables=variables,
                       vartype=vartype, name=name)
