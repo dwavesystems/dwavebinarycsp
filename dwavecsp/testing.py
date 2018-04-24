52a121f4f7f880d3c74b488a952dc512f18fb72e
Alexander Condello <acondello@dwavesys.com>
2018-04-24 13:38:03 -0700

    Update assert_consistent_constraint to match Constraint

--- dwavecsp/testing.py
+++ dwavecsp/testing.py
@@ -19,21 +19,23 @@ def assert_consistent_constraint(const):
 
     assert len(const) == len(const.variables)
 
+    assert hasattr(const, 'vartype')
+    assert isinstance(const.vartype, Vartype)
+
     for config in const.configurations:
         assert isinstance(config, tuple)
         assert len(config) == len(const.variables)
 
+        msg = "config {} does not match constraint vartype '{}'".format(config, const.vartype)
+        assert set(config).issubset(const.vartype.value), msg
+
         assert const.func(*config), 'Constraint.func does not evaluate True for {}'.format(config)
         assert const.check(dict(zip(const.variables, config)))
 
-    if len(const) > 0:
-        assert hasattr(const, 'vartype')
-        assert isinstance(const.vartype, Vartype)
-
-        for config in itertools.product(const.vartype.value, repeat=len(const)):
-            if config in const.configurations:
-                assert const.func(*config)
-                assert const.check(dict(zip(const.variables, config)))
-            else:
-                assert not const.func(*config)
-                assert not const.check(dict(zip(const.variables, config)))
+    for config in itertools.product(const.vartype.value, repeat=len(const)):
+        if config in const.configurations:
+            assert const.func(*config)
+            assert const.check(dict(zip(const.variables, config)))
+        else:
+            assert not const.func(*config)
+            assert not const.check(dict(zip(const.variables, config)))
