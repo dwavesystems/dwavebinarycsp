9b0a130880700e68b0446b44e2c1cf0c79c9f12d
Alexander Condello <acondello@dwavesys.com>
2018-04-25 11:46:54 -0700

    Import package_info into dwavecsp namespace

--- dwavecsp/__init__.py
+++ dwavecsp/__init__.py
@@ -1,15 +1,16 @@
+from dwavecsp.compilers import *
+import dwavecsp.compilers
 
 from dwavecsp.core import *
 import dwavecsp.core
 
-import dwavecsp.factories
-
 import dwavecsp.exceptions
 
-import dwavecsp.testing
+import dwavecsp.factories
 
-from dwavecsp.compilers import *
-import dwavecsp.compilers
+from dwavecsp.package_info import __version__, __author__, __authoremail__, __description__
+
+import dwavecsp.testing
 
 # import dimod.Vartype, dimod.SPIN and dimod.BINARY into dwavecsp namespace for convenience
 from dimod import Vartype, SPIN, BINARY
