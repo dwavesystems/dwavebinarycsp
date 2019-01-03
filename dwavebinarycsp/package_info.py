da93f4aeb88b3d4e5f21fd2ce327442a5fcd24e3
Alexander Condello <acondello@dwavesys.com>
2019-01-03 10:37:07 -0800

    Update version 0.0.9 -> 0.0.10

--- dwavebinarycsp/package_info.py
+++ dwavebinarycsp/package_info.py
@@ -14,7 +14,7 @@
 #
 # ================================================================================================
 
-__version__ = '0.0.9'
+__version__ = '0.0.10'
 __author__ = 'D-Wave Systems Inc.'
 __authoremail__ = 'acondello@dwavesys.com'
 __description__ = 'Solves constraints satisfaction problems with binary quadratic model samplers'
