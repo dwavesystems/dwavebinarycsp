dd985c8bf062c5e6d5968000d12be8d125f2489b
Melody Wong <mwong@dwavesys.com>
2019-10-16 14:16:57 -0700

    Add penaltymodel-lp to setup.py

--- setup.py
+++ setup.py
@@ -30,6 +30,7 @@ else:
 install_requires = [
     'penaltymodel>=0.16.0,<0.17.0',
     'penaltymodel-cache>=0.4.0,<0.5.0',
+    'penaltymodel-lp>=0.1.0,<0.2.0',
     'networkx>=2.0,<3.0',
     'dimod>=0.6.7,<0.9.0',
     'six>=1.11.0,<2.0.0',
