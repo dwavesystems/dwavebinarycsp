a6e5d96a8f3deda9c3c06b1f6aed4c6c863147cd
Alexander Condello <arcondello@gmail.com>
2020-02-24 08:50:11 -0800

    Update version 0.0.12 -> 0.1.0
    Changes
    -------
    * Drop support for Python 2.7 and 3.4

--- dwavebinarycsp/package_info.py
+++ dwavebinarycsp/package_info.py
@@ -14,7 +14,7 @@
 #
 # ================================================================================================
 
-__version__ = '0.0.12'
+__version__ = '0.0.13'
 __author__ = 'D-Wave Systems Inc.'
 __authoremail__ = 'acondello@dwavesys.com'
 __description__ = 'Solves constraints satisfaction problems with binary quadratic model samplers'
