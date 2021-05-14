24c336b87bbdf848e0fc474e8402ff0dab5e5dfd
Joel Pasvolsky <jpasvolsky@dwavesys.com>
2021-05-14 09:32:12 -0700

    Remove Py3.5

--- setup.py
+++ setup.py
@@ -64,13 +64,12 @@ classifiers = [
     'License :: OSI Approved :: Apache Software License',
     'Operating System :: OS Independent',
     'Programming Language :: Python :: 3',
-    'Programming Language :: Python :: 3.5',
     'Programming Language :: Python :: 3.6',
     'Programming Language :: Python :: 3.7',
     'Programming Language :: Python :: 3.8',
 ]
 
-python_requires = '>=3.5'
+python_requires = '>=3.6'
 
 setup(
     name='dwavebinarycsp',
