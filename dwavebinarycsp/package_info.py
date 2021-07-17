ef260bff6d606d8176b287bb6e27a05d6f72de9f
Alexander Condello <arcondello@gmail.com>
2021-07-17 13:08:23 -0700

    Update version 0.1.2 -> 0.1.3
    New Features
    ------------
    * Support `dimod>=0.8.0,<0.11.0`

--- dwavebinarycsp/package_info.py
+++ dwavebinarycsp/package_info.py
@@ -14,7 +14,7 @@
 #
 # ================================================================================================
 
-__version__ = '0.1.2'
+__version__ = '0.1.3'
 __author__ = 'D-Wave Systems Inc.'
 __authoremail__ = 'acondello@dwavesys.com'
 __description__ = 'Solves constraints satisfaction problems with binary quadratic model samplers'
