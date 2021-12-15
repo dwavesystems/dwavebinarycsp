0d877472bda6315c6a31713c553a0e095e091c09
Alexander Condello <arcondello@gmail.com>
2021-12-15 12:45:44 -0800

    Remove comment bars below license headers

--- dwavebinarycsp/io/cnf.py
+++ dwavebinarycsp/io/cnf.py
@@ -11,8 +11,7 @@
 #    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #    See the License for the specific language governing permissions and
 #    limitations under the License.
-#
-# ================================================================================================
+
 """
 The DIMACS format is used to encode boolean satisfiability problems in conjunctive normal form.
 """
