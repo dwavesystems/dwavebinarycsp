bc2614ef59fbcc4e11245cf518e5b1694efcbbb4
Alexander Condello <arcondello@gmail.com>
2024-01-15 17:44:51 -0800

    Test dependency range and unbound install requirements

--- dwavebinarycsp/factories/csp/circuits.py
+++ dwavebinarycsp/factories/csp/circuits.py
@@ -63,12 +63,12 @@ def multiplication_circuit(nbit, vartype=dimod.BINARY):
         to find the product, :math:`p=15` (:math:`001111`).
 
         >>> from dwavebinarycsp.factories.csp.circuits import multiplication_circuit
-        >>> import neal
+        >>> import dwave.samplers
         >>> csp = multiplication_circuit(3)
         >>> bqm = dwavebinarycsp.stitch(csp)
         >>> bqm.fix_variable('a0', 1); bqm.fix_variable('a1', 0); bqm.fix_variable('a2', 1)
         >>> bqm.fix_variable('b0', 1); bqm.fix_variable('b1', 1); bqm.fix_variable('b2', 0)
-        >>> sampler = neal.SimulatedAnnealingSampler()
+        >>> sampler = dwave.samplers.SimulatedAnnealingSampler()
         >>> response = sampler.sample(bqm)
         >>> p = next(response.samples(n=1, sorted_by='energy'))
         >>> print(p['p5'], p['p4'], p['p3'], p['p2'], p['p1'], p['p0'])    # doctest: +SKIP
